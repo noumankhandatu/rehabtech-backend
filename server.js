8ee4becc3ab409d8e5d028eb9ebdf66872820f12
noumankhandatu <noumankhan619.915@gmail.com>
2024-05-23 17:41:36 +0500

    vercel added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { medicinesData } = require("./utils/medicineData");
 const app = express();
 const cookieParser = require("cookie-parser");
 const { MedicineRoute } = require("./routes/Medicine-route");
@@ -12,6 +11,10 @@ app.use(express.json());
 app.use(cookieParser());
 // Sample medicine data
 app.use("/api/v1", MedicineRoute);
+
+app.get('/', (req, res) => {
+  return res.send('Very well , Api`s are up and running ')
+})
 
 const port = 8000;
 app.listen(port, () => {
