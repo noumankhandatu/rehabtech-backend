c1d5806950b77ebdfa4bb095f453236db30b796f
noumankhandatu <noumankhan619.915@gmail.com>
2024-05-28 02:52:11 +0500

    get medicine by id

--- routes/Medicine-route.js
+++ routes/Medicine-route.js
@@ -3,7 +3,7 @@ const MedicineRoute = express.Router();
 const { GetAllMedicines, GetMedicineById } = require("../controller/medicineController");
 
 MedicineRoute.get("/all-medicines", GetAllMedicines);
-MedicineRoute.get("/medicines/:id", GetMedicineById);
+MedicineRoute.get("/all-medicines/:id", GetMedicineById);
 
 
 module.exports = { MedicineRoute };
